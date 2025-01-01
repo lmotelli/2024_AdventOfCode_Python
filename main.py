2df9bb12136c4c47018c8f432ed85d282022ef00
Lorenzo Motelli <lorenzo.motelli97@gmail.com>
2025-01-01 23:00:45 +0100

    environment preparation

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 def main():
-    testArray = [1,2,3,4,5,6,7,8,9]
-    testSubarray = testArray[:5]
-    testOtherSubarray = testArray[5+1:]
-    print(testSubarray)
-    print(testOtherSubarray)
-    testArray.pop(5)
-    print(testArray)
-    print(testArray.pop(5))
+    for i in range(5, 6):
+        with open("C:\Users\loren\.vscode\2024_AdventOfCode_Python\2024_AdventOfCode_Python\inputs\test{i}.txt", 'w'):
+            pass
     
 main()
